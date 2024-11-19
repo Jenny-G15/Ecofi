36a0a8f22f8a978280e6d98573f51d951b5792e2
MarilaMorales <mmorales@fwdcostarica.com>
2024-11-19 11:29:28 -0600

    Se crearon todas las Paginas con su CSS

--- FE_Ecofi/src/pages/Principal.jsx
+++ FE_Ecofi/src/pages/Principal.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import "../Componentes/Contactenos"
-import ContactForm from '../Componentes/Contactenos'
 import Nav from '../Componentes/Nav'
 import Footer from '../Componentes/Footer'
 import "../Styles/Principal.css"

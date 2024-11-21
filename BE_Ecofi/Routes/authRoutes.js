bdec52bd39b46741881d20f82a5e23dceda2a219
Jenny-G15 <jguadamuz@fwdcostarica.com>
2024-11-21 15:21:02 -0600

    milddleware

--- BE_Ecofi/Routes/authRoutes.js
+++ BE_Ecofi/Routes/authRoutes.js
@@ -6,3 +6,16 @@ const router = express.Router();
 router.post('/login', iniciarSesion);
 
 module.exports = router;
+
+
+// const express = require('express');
+// const router = express.Router();
+// const { verificarToken } = require('../middlewares/authMiddleware');
+
+// // Ruta protegida, solo accesible si se tiene un token válido
+// router.get('/perfil', verificarToken, (req, res) => {
+//     // La solicitud llega aquí solo si el token es válido
+//     res.status(200).json({ message: 'Acceso autorizado', usuario: req.usuario });
+// });
+
+// module.exports = router;

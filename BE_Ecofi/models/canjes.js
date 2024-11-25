ef087a26f0bdfa56cc1e67568149e5fba9cb8d05
MarilaMorales <mmorales@fwdcostarica.com>
2024-11-25 15:17:14 -0600

    Correcion Modelo

--- BE_Ecofi/models/canjes.js
+++ BE_Ecofi/models/canjes.js
@@ -1,62 +1,74 @@
 'use strict';
 
-const { Model, DataTypes} = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
 
   class Canjes extends Model {
-    
-    
+
     static associate(models) {
+      // Relación Canjes - Usuario
       Canjes.belongsTo(models.Usuario, {
-        foreignKey: 'ID_Usuario', 
-        as: 'usuarioCanjes', 
+        foreignKey: 'ID_Usuario',
+        as: 'usuarioCanjes',
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       });
-      
+
+      // Relación Canjes - Producto
       Canjes.belongsTo(models.Producto, {
-        foreignKey: 'ID_Producto', 
+        foreignKey: 'ID_Producto',
         as: 'productosCanjes',
         onUpdate: 'CASCADE',
         onDelete: 'CASCADE',
       });
 
+      // Relación opcional con Historial_Canjes (solo algunos Canjes pueden tener un Historial)
       Canjes.belongsTo(models.Historial_Canjes, {
-        foreignKey: 'ID_Canje',
+        foreignKey: 'ID_Historial_Canje', // Usamos otro nombre para evitar ciclo
         as: 'historialCanje',
-        onUpdate:'CASCADE',
-        onDelete:'CASCADE',  
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',  // Si no hay historial, la referencia se pone en NULL
+        allowNull: true, // Permitir que no todos los canjes tengan historial
       });
-      
     }
   }
+
   Canjes.init({
     ID_Usuario: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: 'Usuarios',
-        key: 'id'
-      }
+        key: 'id',
+      },
     },
     ID_Producto: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
         model: 'Productos',
-        key: 'id'
-      }
+        key: 'id',
+      },
     },
     Fecha_Canje: {
       type: DataTypes.DATE,
-      allowNull: false
+      allowNull: false,
     },
+    ID_Historial_Canje: {
+      type: DataTypes.INTEGER,
+      allowNull: true, // Historial es opcional, un canje no necesariamente tiene que tener historial
+      references: {
+        model: 'Historial_Canjes', // Relacionamos con Historial_Canjes
+        key: 'id', // ID del Historial_Canjes
+      },
+    }
   }, {
     sequelize,
     modelName: 'Canjes',
     tableName: 'Canjes',
     timestamps: true,
   });
+
   return Canjes;
 };

a03ef5f340d2e7514b69db219ace2514f7bc5d35
MarilaMorales <mmorales@fwdcostarica.com>
2024-12-04 13:52:53 -0600

    Instalacion de react-leaflet y leaflet

--- FE_Ecofi/src/App.jsx
+++ FE_Ecofi/src/App.jsx
@@ -2,6 +2,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import 'leaflet/dist/leaflet.css';
+
 
 import Routing from "./routes/Routing"; // Asegúrate de que la ruta esté bien
 

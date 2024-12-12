bb073c5e0426514c88c39bcd0903b71ae6d59f8a
MarilaMorales <mmorales@fwdcostarica.com>
2024-12-12 08:35:23 -0600

    Cambios en las Cards de la pagina principal

--- BE_Ecofi/index.js
+++ BE_Ecofi/index.js
@@ -46,6 +46,6 @@ app.use('/recofi', recofiRoutes);
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://192.168.1.246:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
     
 });

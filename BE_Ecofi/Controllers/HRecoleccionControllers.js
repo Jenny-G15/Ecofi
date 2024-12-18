92a46e924f1afa6000b43517f6d7ad3eba31a426
MarilaMorales <mmorales@fwdcostarica.com>
2024-12-18 08:36:22 -0600

    cambios en historial recoleccion

--- BE_Ecofi/Controllers/HRecoleccionControllers.js
+++ BE_Ecofi/Controllers/HRecoleccionControllers.js
@@ -90,134 +90,66 @@ const eliminarHistorialRecoleccion = async (req, res) => {
 };
 
 
-
-
-
 const obtenerEstadisticas = async (req, res) => {
   try {
-    // RECOFI con más intercambios (usuario con mayor cantidad recolectada)
-    const recofiConMasRecolecciones = await Formulario.findOne({
+    // RECOFI con más intercambios
+    const recofiConMasIntercambios = await Formulario.findOne({
       attributes: [
         'ID_Recofi',
-        [Sequelize.fn('SUM', Sequelize.col('HistorialRecoleccion.Cantidad_Hrecoleccion')), 'totalRecolectado']
+        [Sequelize.fn('COUNT', Sequelize.col('ID_Recofi')), 'totalIntercambios']
       ],
       include: [
         {
-          model: Historial_Recoleccion,
-          as: 'HistorialRecoleccion',
-          attributes: [] // No necesitamos traer atributos del historial
-        },
-        {
           model: Recofi,
           as: 'Recofi',
-          attributes: ['Nombre_Recofi'] // Nombre del recofi
+          attributes: ['Nombre_Recofi']
         }
       ],
       group: ['ID_Recofi', 'Recofi.id'],
-      order: [[Sequelize.literal('totalRecolectado'), 'DESC']],
+      order: [[Sequelize.literal('totalIntercambios'), 'DESC']],
       limit: 1
     });
 
+
+    
     // Material más intercambiado
     const materialMasIntercambiado = await Formulario.findOne({
       attributes: [
         'ID_Material',
-        [Sequelize.fn('SUM', Sequelize.col('HistorialRecoleccion.Cantidad_Hrecoleccion')), 'totalRecolectado']
+        [Sequelize.fn('COUNT', Sequelize.col('ID_Material')), 'totalIntercambios']
       ],
       include: [
         {
-          model: Historial_Recoleccion,
-          as: 'HistorialRecoleccion',
-          attributes: [] // No necesitamos traer atributos del historial
-        },
-        {
           model: Material,
           as: 'Material',
-          attributes: ['Nombre_Material'] // Nombre del material
+          attributes: ['Tipo_Material']
         }
       ],
       group: ['ID_Material', 'Material.id'],
-      order: [[Sequelize.literal('totalRecolectado'), 'DESC']],
+      order: [[Sequelize.literal('totalIntercambios'), 'DESC']],
       limit: 1
     });
 
-    // Enviar la respuesta con los resultados
+    // Respuesta con los datos obtenidos
     res.json({
-      recofiTop: recofiConMasRecolecciones || 'No hay datos disponibles',
-      materialTop: materialMasIntercambiado || 'No hay datos disponibles'
+      recofiTop: recofiConMasIntercambios
+        ? {
+            nombre: recofiConMasIntercambios.Recofi.Nombre_Recofi,
+            totalIntercambios: recofiConMasIntercambios.get('totalIntercambios')
+          }
+        : 'No hay datos disponibles',
+      materialTop: materialMasIntercambiado
+        ? {
+            nombre: materialMasIntercambiado.Material.Tipo_Material,
+            totalIntercambios: materialMasIntercambiado.get('totalIntercambios')
+          }
+        : 'No hay datos disponibles'
     });
   } catch (error) {
-    // Capturar errores y devolver mensaje amigable
     console.error('Error al obtener las estadísticas:', error);
     res.status(500).json({ error: 'Ocurrió un problema al obtener las estadísticas' });
   }
 };
-
-
-
-
-
-
-
-// const obtenerEstadisticas = async (req, res) => {
-//   try {
-//     // RECOFI con más intercambios
-//     const recofiTop = await Formulario.findAll({
-//       attributes: [
-//         'ID_Recofi',
-//         [Sequelize.fn('SUM', Sequelize.col('formularioHRecoleccion.Cantidad_Hrecoleccion')), 'totalRecolecciones'],
-//       ],
-//       include: [
-//         {
-//           model: Historial_Recoleccion,
-//           as: 'formularioHRecoleccion',
-//           attributes: [],
-//         },
-//         {
-//           model: Recofi,
-//           as: 'formularioRecofi',
-//           attributes: ['Nombre_Recofi'],
-//         },
-//       ],
-//       group: ['ID_Recofi', 'formularioRecofi.id'],
-//       order: [[Sequelize.literal('totalRecolecciones'), 'DESC']],
-//       limit: 1,
-//     });
-
-//     // Material más intercambiado
-//     const materialTop = await Formulario.findAll({
-//       attributes: [
-//         'ID_Material',
-//         [Sequelize.fn('SUM', Sequelize.col('formularioHRecoleccion.Cantidad_Hrecoleccion')), 'totalRecolecciones'],
-//       ],
-//       include: [
-//         {
-//           model: Historial_Recoleccion,
-//           as: 'formularioHRecoleccion',
-//           attributes: [],
-//         },
-//         {
-//           model: Material,
-//           as: 'materialFormulario',
-//           attributes: ['Nombre_Material'],
-//         },
-//       ],
-//       group: ['ID_Material', 'materialFormulario.id'],
-//       order: [[Sequelize.literal('totalRecolecciones'), 'DESC']],
-//       limit: 1,
-//     });
-
-//     res.json({
-//       recofiTop: recofiTop[0],
-//       materialTop: materialTop[0],
-//     });
-//   } catch (error) {
-//     console.error('Error al obtener estadísticas:', error);
-//     res.status(500).json({ error: 'Error al obtener estadísticas' });
-//   }
-// };
-
-
 
 
 module.exports = { obtenerHistorialesRecoleccion, crearHistorialRecoleccion, actualizarHistorialRecoleccion, eliminarHistorialRecoleccion,  obtenerEstadisticas };

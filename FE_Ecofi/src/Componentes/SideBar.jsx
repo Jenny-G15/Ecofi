a9fecf0e5d30125ee5f77a73066c14105f23b004
MarilaMorales <mmorales@fwdcostarica.com>
2024-12-20 15:01:30 -0600

    Creacion de la nueva Tabla AdminRecofi

--- FE_Ecofi/src/Componentes/SideBar.jsx
+++ FE_Ecofi/src/Componentes/SideBar.jsx
@@ -9,7 +9,9 @@ function AdminSideBar() {
   return (
     <div className="sidebarContainer">
       <nav className="sidebarNav">
-        <Link className="sidebarLink" to="/Administracion">
+        <h4 id="tituloSideBar" >Menú</h4>
+        <hr id="hrSideBar"/>
+        <Link id="SideText" className="sidebarLink" to="/Administracion">
           Agregar Productos
         </Link>
         <Link className="sidebarLink" to="/AgregarAdmin">
@@ -35,25 +37,3 @@ function AdminSideBar() {
 
 export default AdminSideBar;
 
-
-
-
-
-// import Nav from 'react-bootstrap/Nav';
-// import "../Styles/Sidebar.css";
-// import { Link } from "react-router-dom"; 
-
-// function SideBar() {
-//   return (
-//     <Nav defaultActiveKey="/home" className="sidebar flex-column">
-//       <Nav.Item>
-//         <Link id='linkAdminUnique' to="/Productos">Agregar Productos</Link>
-//       </Nav.Item>
-//       <Nav.Item>
-//         <Link id='linkImagesUnique' to="/AgregarRecofi">Agregar Recofis</Link>
-//       </Nav.Item>
-//     </Nav>
-//   );
-// }
-
-// export default SideBar;

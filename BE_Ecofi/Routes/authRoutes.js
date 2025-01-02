570b52f88a149c1de4eea73e62a5cb07a108e8f6
MarilaMorales <mmorales@fwdcostarica.com>
2025-01-02 15:13:44 -0600

    Cambios Git

--- BE_Ecofi/Routes/authRoutes.js
+++ BE_Ecofi/Routes/authRoutes.js
@@ -1,31 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { obtenerUsuarios, obtenerUsuariosxCedula, registrarUsuario, iniciarSesion, eliminarUsuario, actualizarUsuario } = require('../controllers/authControllers');
-const {forgotPassword, resetPassword} = require('../Controllers/authControllers')
-const { iniciarSesion, registrarUsuario, obtenerUsuarios, eliminarUsuario, actualizarUsuario, actualizarBicolones } = require('../Controllers/authControllers');
-// const {forgotPassword, resetPassword} = require('../Controllers/authControllers')
+const { obtenerUsuarios, obtenerUsuariosxCedula, registrarUsuario, iniciarSesion, eliminarUsuario, actualizarUsuario, actualizarBicolones} = require('../controllers/authControllers');
+
 
 
 router.get('/', obtenerUsuarios); 
 router.get('/:cedula', obtenerUsuariosxCedula);
 router.post('/login', iniciarSesion); 
 router.post('/register', registrarUsuario);
-
 router.delete('/:id', eliminarUsuario); 
 router.put('/:id', actualizarUsuario); 
-
 router.delete('/:id', eliminarUsuario);
 router.put('/:id', actualizarUsuario);
 router.put('/usuarios/:id/bicolones', actualizarBicolones);
 
 
 
-
-
-
-// Recuperación de contraseña
-router.post('/forgotPassword', forgotPassword);
-router.post('/resetPassword', resetPassword);
 
 
 module.exports = router;

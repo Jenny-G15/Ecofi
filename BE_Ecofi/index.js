c0a04c12085a9ef14ed04c75e8ac945b0ae31946
MarilaMorales <mmorales@fwdcostarica.com>
2025-01-07 10:17:55 -0600

    Cambio de localhost a IP

--- BE_Ecofi/index.js
+++ BE_Ecofi/index.js
@@ -47,6 +47,6 @@ app.use('/adminRecofi', AdminRecofiRoutes)
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://192.168.1.246:${PORT}`);
     
 });
